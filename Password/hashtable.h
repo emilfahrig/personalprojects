bd8bb7dbed0454e16f35dda77f7295d6b4d7fb18
Emil Fahrig <69064149+emilfahrig@users.noreply.github.com>
2020-08-24 17:25:53 -0700

    Update hashtable.h

--- Password/hashtable.h
+++ Password/hashtable.h
@@ -30,40 +30,6 @@ class HashTable {
         QString getEmail(int key);
         void changePassword(int key_input, QString email_input, int new_encryption);
         bool checkAuthenticityChangePassword(int key_input, QString email_input);
-        //description: this function adds a node to the hash table storing in
-        //it the given object and key at the appropriate index
-        //preconditions: a Movie object has been created in main
-        //postconditions: a node has been added to the hash table that stores
-        //the passed in data
-        //bool watchMovie(long key);
-        //description: this function deletes the node in the hash table that
-        //stores the passed in key
-        //preconditions: a node must exist in the hash table that stores the
-        //passed in key
-        //postconditions: the node that stores the passed in key is deleted
-        //from the linked list
-        //void displayMovie(long key);
-        //description: displays the information of the movie with the passed
-        //in key
-        //preconditions: a movie with the passed in key must exist in the hash
-        //table for anything to be printed to the console
-        //postoncitions: no member variables will be altered after this
-        //function runs
-        //void displayAll();
-        //description: this function prints the title and id stored in each
-        //node in the hash table
-        //preconditions: nodes must have been added to the hash table for this
-        //function to print
-        //postoncitions: no member variables will be altered after this
-        //function runs
-        //void displayGenre(string genre);
-        //description: this function prints the title and id stored in each
-        //node in the hash table that stores a Movie object with the passed
-        //in genre
-        //preconditions: nodes must have been added to the hash table that
-        //store Movie objects which store the passed in genre
-        //postoncitions: no member variables will be altered after this
-        //function runs
 };
 
 #endif // ARRAY_H
